3db904de587c7e83c47b630f73383db1fd0a12f7
Alaukik Miishra <151136125+Alaukik1210@users.noreply.github.com>
2025-08-29 16:17:52 +0530

    Update ReportFormCompleted.tsx

--- components/report/ReportFormCompleted.tsx
+++ components/report/ReportFormCompleted.tsx
@@ -51,7 +51,8 @@ export function ReportSubmitted({ data }: ReportSubmittedProps) {
 
       <div className="pt-4">
         <button
-          onClick={() => (window.location.href = "/")}
+          onClick={() => router.push("/")}
+
           className="inline-flex items-center justify-center rounded-lg cursor-pointer  bg-sky-500 px-4 py-2 text-sm font-medium text-white hover:bg-sky-400"
         >
           Return to Home
